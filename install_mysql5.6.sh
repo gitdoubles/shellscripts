05f24b51fe56ec65fc07e55f306d23fd3a1b5e81
doubles <doubles159@163.com>
2018-01-26 23:21:06 +0800

    sucessfull version

--- install_mysql5.6.sh
+++ install_mysql5.6.sh
@@ -2,10 +2,12 @@
 
 GROUP_NAME=mysql  
 USER_NAME=mysql  
-MYSQLDB_HOME=/usr/local/mysql  
-MYSQLDB_DATA_HOME=/data/mysql/
+MYSQLDB_BASE_HOME="/usr/local/mysql"  
+MYSQLDB_DATA_HOME="/data/mysql"
+MYSQLDB_PORT=3307
 MYSQL_VERSION="mysql-5.6.37-linux-glibc2.12-x86_64"
-MYSQL_START_SCRIPTNAME=mysql5.6
+VERSION_NUM=`echo $MYSQL_VERSION|awk -F - '{print $2}'`
+MYSQL_START_SCRIPTNAME=mysql
 ERROR_EXIT=65
 
 #==============================
@@ -34,7 +36,7 @@ Add_user()
     # addGroup    
     if [ -z $(cat /etc/group|awk -F: '{print $1}'| grep -w "$GROUP_NAME") ]  
     then  
-         groupadd  $GROUP_NAME  
+         groupadd -g 27 $GROUP_NAME  
         if(( $? == 0 ))  
         then  
             echo "group $GROUP_NAME add sucessfully!"  
@@ -46,7 +48,7 @@ Add_user()
     # addUser    
     if [ -z $(cat /etc/passwd|awk -F: '{print $1}'| grep -w "$USE_NAME") ]  
     then  
-        adduser -g $GROUP_NAME $USER_NAME  
+        adduser  -u 27 -g $GROUP_NAME $USER_NAME  
         if (( $? == 0 ))  
         then  
             echo "user $USER_NAME add sucessfully!"  
@@ -79,40 +81,42 @@ Add_dir()
 {    
     cd /tmp  
     tar xzvf ${MYSQL_VERSION}.tar.gz  
-    # if the basedir ${MYSQLDB_HOME} is exsits.....
-    if [ -d "${MYSQLDB_HOME}" ]
+    # if the basedir ${MYSQLDB_BASE_HOME} is exsits.....
+    if [ -d "${MYSQLDB_BASE_HOME}" ]
     then
-        echo "The basedir ${MYSQLDB_HOME} is exsits"
-        if [ -d "/usr/local/${MYSQL_START_SCRIPTNAME}" ]
-        then
-            echo "The basedir /usr/local/${MYSQL_START_SCRIPTNAME} also exsits,please modify the basedir..."
-            exit 1
-        else
-            MYSQLDB_HOME=/usr/local/$MYSQL_START_SCRIPTNAME
-            mv ${MYSQL_VERSION} ${MYSQLDB_HOME}
-        fi
+        echo "The basedir ${MYSQLDB_BASE_HOME} is exsits"
+	DIR_NUM=1
+    	MYSQLDB_BASE_HOME_TMP="${MYSQLDB_BASE_HOME}-$VERSION_NUM"
+	MYSQLDB_DATA_HOME_TMP="${MYSQLDB_DATA_HOME}-$VERSION_NUM"
+	MYSQL_START_SCRIPTNAME_TMP="${MYSQL_START_SCRIPTNAME}-$VERSION_NUM"
+	MYSQLDB_PORT=$((MYSQLDB_PORT + 1))
+	while [ -d  $MYSQLDB_BASE_HOME_TMP ]
+    	do
+		echo "The basedir ${MYSQLDB_BASE_HOME_TMP} also exsits"
+        	MYSQLDB_BASE_HOME_TMP="${MYSQLDB_BASE_HOME_TMP}_${DIR_NUM}"
+		MYSQLDB_DATA_HOME_TMP="${MYSQLDB_DATA_HOME_TMP}_${DIR_NUM}"
+		MYSQL_START_SCRIPTNAME_TMP="${MYSQL_START_SCRIPTNAME_TMP}_${DIR_NUM}"
+        	DIR_NUM=$((DIR_NUM + 1))
+    	done
+    	MYSQLDB_BASE_HOME=$MYSQLDB_BASE_HOME_TMP
+	MYSQLDB_DATA_HOME=$MYSQLDB_DATA_HOME_TMP
+	MYSQL_START_SCRIPTNAME=$MYSQL_START_SCRIPTNAME_TMP
+        mv ${MYSQL_VERSION} ${MYSQLDB_BASE_HOME}
     else
-        mv ${MYSQL_VERSION} ${MYSQLDB_HOME}
+        mv ${MYSQL_VERSION} ${MYSQLDB_BASE_HOME}
     fi
-    # if the datadir ${MYSQLDB_HOME} is exsits.....
+    # if the datadir ${MYSQLDB_DATA_HOME} is exsits.....
     if [ -d "${MYSQLDB_DATA_HOME}" ]
     then
-        echo "The datadir $MYSQLDB_DATA_HOME is exsits"
-        if [ -d "/data/${MYSQL_START_SCRIPTNAME}" ]
-        then
-            echo "The datadir /data/${MYSQL_START_SCRIPTNAME} also exsits,please modify the datadir..."
-            exit 1
-        else
-            MYSQLDB_DATA_HOME=/data/$MYSQL_START_SCRIPTNAME
-            mkdir -p ${MYSQLDB_DATA_HOME}
-        fi 
+        echo "The datadir $MYSQLDB_DATA_HOME also exsits,please modify the datadir..."
+	exit $ERROR_EXIT
     else
         mkdir -p ${MYSQLDB_DATA_HOME}
     fi
 
     # add privileges to the basedir and datadir
-    chown -R USER_NAME:GROUP_NAME ${MYSQLDB_HOME}
-    chown -R USER_NAME:GROUP_NAME ${MYSQLDB_DATA_HOME}
+    chown -R $USER_NAME:$GROUP_NAME ${MYSQLDB_BASE_HOME}
+    chown -R $USER_NAME:$GROUP_NAME ${MYSQLDB_DATA_HOME}
 }
 
 #==============================
@@ -120,8 +124,9 @@ Add_dir()
 #==============================
 Install_mysql()
 {
+    yum install -y perl-Module-Install.noarch
     # initialize the mysql
-    ${MYSQLDB_HOME}/scripts/mysql_install_db --user=$USER_NAME --datadir=${MYSQLDB_DATA_HOME} --basedir=${MYSQLDB_HOME}
+    ${MYSQLDB_BASE_HOME}/scripts/mysql_install_db --user=$USER_NAME --datadir=${MYSQLDB_DATA_HOME} --basedir=${MYSQLDB_BASE_HOME}
     if(( $? == 0 ))
     then
         echo "MySQL Initialize sucessfully!"   
@@ -131,13 +136,14 @@ Install_mysql()
     fi
 
     # modify the my.cnf
-    cd ${MYSQLDB_HOME}
-    if [ -s ./my.cnf ];then
+    cd ${MYSQLDB_BASE_HOME}
+    if [ -s my.cnf ];then
         echo "the my.cnf is exsits,it will be overwrite!"
-        echo"[mysqld]
- basedir = $MYSQLDB_HOME
+        echo "
+[mysqld]
+ basedir = $MYSQLDB_BASE_HOME
  datadir = $MYSQLDB_DATA_HOME
- port = 3307
+ port = $MYSQLDB_PORT
  socket = ${MYSQLDB_DATA_HOME}/mysql.sock
  innodb_file_per_table=1
  default-storage-engine=INNODB
@@ -149,16 +155,21 @@ Install_mysql()
 log-error=${MYSQLDB_DATA_HOME}/mysqld.log
 pid-file=${MYSQLDB_DATA_HOME}/mysqld.pid" >./my.cnf
     fi
-    if [ -s /etc/my.cnf ]; then  
-        mv /etc/my.cnf /etc/my.cnf.`date +%Y%m%d%H%M%S`.bak  
-    fi 
-    cp ${MYSQLDB_HOME}/my.cnf /etc/my.cnf
-
+    [ $? -eq 0 ] && echo "my.cnf created successfull!!"
+    #if [ -s /etc/my.cnf ]; then  
+    #    mv /etc/my.cnf /etc/my.cnf.`date +%Y%m%d%H%M%S`.bak  
+    #fi 
+    #cp ${MYSQLDB_BASE_HOME}/my.cnf /etc/my.cnf
+    #if(( $? == 0 ))
+    #then
+    #    echo "/etc/my.cnf created  sucessfully!"   
+    #else
+    #    echo "/etc/my.cnf created failed!"  
+    #fi
     # add the bin to the path
-    echo "add the $MYSQLDB_HOME/bin to the path!"
+    echo "add the $MYSQLDB_BASE_HOME/bin to the path!"
     cat >> /etc/profile <<EOF  
-export MYSQL_HOME="/usr/local/mysql5.6/"
-export PATH="$PATH:$MYSQL_HOME/bin"
+export PATH="$PATH:${MYSQLDB_BASE_HOME}/bin"
 EOF
     source /etc/profile
 }
@@ -169,7 +180,9 @@ Start_mysql()
 {
     # add the service to the system service directory
     echo "Add the service to the system as mysql5.6,you can start the service by the command 'service mysql5.6 start' !"
-    cp ${MYSQLDB_HOME}/support-files/mysql.server /etc/init.d/$MYSQL_START_SCRIPTNAME
+    cp ${MYSQLDB_BASE_HOME}/support-files/mysql.server /etc/init.d/$MYSQL_START_SCRIPTNAME
+    sed -i "s!^basedir=.*!basedir=$MYSQLDB_BASE_HOME!g" /etc/init.d/$MYSQL_START_SCRIPTNAME
+    sed -i "s!^datadir=.*!datadir=$MYSQLDB_DATA_HOME!g" /etc/init.d/$MYSQL_START_SCRIPTNAME
     # add to the system service control and it will auto start when the system restart!
     chkconfig --add $MYSQL_START_SCRIPTNAME
     chkconfig $MYSQL_START_SCRIPTNAME on
@@ -190,13 +203,15 @@ Create_user()
     fi
     # create mysql's user and set root's password
     read -p "Input Default Mysql user root's password: " MYSQL_PASSWORD
-    ${MYSQLDB_HOME}/bin/mysql $MYSQL_CONNECT_PARAMETER  <<EOF 
+    HOST_NAME=`hostname`
+    ${MYSQLDB_BASE_HOME}/bin/mysql $MYSQL_CONNECT_PARAMETER  <<EOF 
     select Host,User,Password from mysql.user;
     CREATE USER 'roottest1'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
     GRANT ALL PRIVILEGES ON *.* TO'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;
     update mysql.user set password=password('$MYSQL_PASSWORD') where User="root" and Host="localhost";
     update mysql.user set password=password('$MYSQL_PASSWORD') where User="root" and Host="127.0.0.1";
     update mysql.user set password=password('$MYSQL_PASSWORD') where User="root" and Host="::1";
+    update mysql.user set password=password('$MYSQL_PASSWORD') where User="root" and Host="$HOST_NAME";
     flush privileges;
     select Host,User,Password from mysql.user;
 EOF
@@ -209,7 +224,7 @@ Main()
 {   
     Echo_motd;
     Add_user;
-    Down_mysql;
+#    Down_mysql;
     Add_dir;
     Install_mysql;
     Start_mysql;
